6efe75fcdeb6ecface07cf3d728d71a69eeb6216
Sasha <usashinetssd@gmail.com>
2021-10-27 17:44:11 +0300

    lesson 18

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
-const numberOfFilms = +prompt('How old are you?', '23');
+'use strict';
 
-const bussiinesss = {
-    age: numberOfFilms,
-    job: {},
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '');
+    }
+}
+
+//start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
     actors: {},
     genres: [],
     private: false
 };
 
-for (let i = 0; i < 2; i++) {
-        const a = prompt('Whats your job?', ''),
-        b = prompt('How much money do you make?', 0);
+function rememberMuFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('Насколько Вы его ещениваете?', 0);
 
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        bussiinesss.job[a] = b;
+        personalMovieDB.movies[a] = b;// didnt work
         console.log('Done');
     } else {
         console.log('Error');
         i--;
     }
 
+    }
 }
 
-console.log(bussiinesss);
+//rememberMuFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 5) { //Использвать switch здесь не удасться, поскольку он со строгим условием
+        console.log('Вы нормальный человек.');
+    } else if (personalMovieDB.count >= 5 && personalMovieDB.count < 10) {
+        console.log('Есть подозрение, что Вы больны киноманией!');
+    } else if (personalMovieDB.count >= 10) {
+        console.log('Вы больны киноманией! Срочно попкорн!');
+    } else {
+        console.log('Произошла ошибка ввода');
+    }
+}
+
+//detectPersonalLevel();
+
+function showMyDB(hidden) {
+   if (!hidden) {
+       console.log(personalMovieDB);
+   }
+}
+
+//showMyDB(personalMovieDB.private);
+
+function writeYourGenres() {
+    for (let i = 1;i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+console.log(personalMovieDB);
 
 
---
+++ script1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function showFirstMassage(text) {
+    console.log(text);
+    let num = 20;
+}
+
+showFirstMassage('text');
+
+function calc(a, b) {
+    return (a + b);
+}
+
+
+console.log(calc(4, 8));
+
+const gr = "sfsgg";
+console.log(gr.toUpperCase());
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 2));
